27f9ada4136a206b832d744c98d66c05262c5b8e
Binaryify <zhuangtongfa@qq.com>
2016-05-14 10:45:51 +0800

    improve

---
+++ app/action.js
@@ -0,0 +1,16 @@
+export function hah(){
+  return {
+    type:"ADD"
+  }
+}
+export function hah2(){
+  return {
+    type:"INCREMENT"
+  }
+}
+export const hah3=(text)=>{
+  return {
+    type:"PPP",
+    text
+  }
+}
